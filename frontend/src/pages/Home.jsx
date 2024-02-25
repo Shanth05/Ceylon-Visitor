f2b3f29aae40d1ef8cb72e52f00a05f5b6fb1454
Pirashanth <shanth.pirashanth5@gmail.com>
2024-02-25 22:36:37 +0530

    Update

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -5,8 +5,10 @@ import { Container, Row,Col} from 'reactstrap' ;
 import heroImg from '../assets/images/hero-img01.jpg'
 import heroImg02 from '../assets/images/hero-img02.jpg'
 import heroVideo from '../assets/images/hero-video.mp4'
-import worlding from '../assets/images/world.png'
+import worldImg from '../assets/images/world.png'
 import Subtitle from './../shared/Subtitle';
+
+import SearchBar from '../shared/SearchBar';
 
 const Home = () => {
   return <>
@@ -19,21 +21,44 @@ const Home = () => {
           <div className="hero__content">
             <div className="hero__subtitle  d-flex align-items-center ">
               <Subtitle subtitle={'Know Before You Go'}/>
-              <img src={worlding} alt="" />
+              <img src={worldImg} alt="" />
             </div>
-            <h1>Travelling opens the door to crating <span 
-            className="highlight"> memories</span>
-            </h1>
+            <h1>Traveling opens the door to creating <span
+            className="highlight"> memories</span></h1>
             <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
               Similique ab temporibus sit illo tempore eveniet! Voluptatibus exercitationem minus nulla quas tempora! Distinctio, natus? Beatae, omnis.
               Reprehenderit placeat quos fugiat dicta?
             </p>
           </div>
         </Col>
+        <Col lg='2'>
+            <div className='hero__img-box'> 
+              <img src={heroImg} alt="" />
+            </div>
+          </Col>
+          <Col lg='2'>
+            <div className='hero__img-box mt-4'> 
+              <video src={heroVideo} alt="" controls/>
+            </div>
+          </Col>
+          <Col lg='2'>
+            <div className='hero__img-box mt-5'> 
+              <img src={heroImg02} alt="" />
+            </div>
+          </Col>
+          <SearchBar />
       </Row>
     </Container>
   </section>
     {/*====== hero section start ===== */}
+    <section>
+      <Container>
+        <Col lg='3'>
+          <h5 className="services__subtitle"> What we serve </h5>
+          <h2 className="services__title"> We Offer our best services</h2>
+        </Col>
+      </Container>
+    </section>
   
   </>
 };
