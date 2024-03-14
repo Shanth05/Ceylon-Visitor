41b2b331d6296115ac6a84fba814b76d3b87c373
Pirashanth <shanth.pirashanth5@gmail.com>
2024-03-14 23:22:51 +0530

    FEAT:Fetch

--- frontend/src/hooks/useFetch.js
+++ frontend/src/hooks/useFetch.js
@@ -1,38 +1,38 @@
 import { useState, useEffect } from 'react'
 
 const useFetch = (url) => {
+    
+    const [data,setData] = useState([])
+    const [error,setError] = useState([null])
+    const [loading,setLoading] = useState([false])
 
-    const [data,setdata] = useState([])
-    const [error,setError] = useState(null)
-    const [loading,setLoading] =useState(false)
+    useEffect (()=> {
+        const fetchData = async() => {
+            setLoading(true)
 
-    useEffect(() =>{
-        const fetchData = async()=>
-        setLoading(true)
+            try {
+                const res = await fetch(url)
 
-        try {
-            const res = await fetch(url)
+                if(!res.ok){
+                    setError('failed to fetch')
+                }
+                const result = await res.json()
+                setData(result.data)
 
-            if(!res.ok){
-                setError('failed to fetch')
-                alert('failed to fetch')
+            } catch (err) {
+                setError(err.message)
+                setLoading(false)
             }
-            const result = await res.json()
-            setdata(result.data)
-        
-        } catch(err) {
-            setError(err.message)
-            alert(false)
         }
-
         fetchData()
-    }, [url])
+    },[url])
 
-    return {
+    return{
         data,
         error,
         loading,
     }
+    
 }
 
 export default useFetch
