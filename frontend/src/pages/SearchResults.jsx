24a3723e4fb73368a4dee3a937b7f3a18258599e
Pirashanth <shanth.pirashanth5@gmail.com>
2024-03-24 23:15:31 +0530

    FEAT:SearchResult

--- frontend/src/pages/SearchResults.jsx
+++ frontend/src/pages/SearchResults.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React, {useState} from 'react'
+
+import CommonSection from './../shared/CommonSection'
+import { Container, Row, Col } from 'reactstrap'
+
+import { useLocation } from 'react-router-dom'
+import TourCard from './../shared/TourCard'
 
 const SearchResults = () => {
-  return (
-    <div>SearchResults</div>
-  )
+  return
+  <>
+  <CommonSection title={'Tour Search Result'} />
+  <section>
+    <Container>
+
+    </Container>
+  </section>
+  </>
 }
 
 export default SearchResults;
